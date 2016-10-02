92b7836e03a74f8f9a9b951d980cd950f8dcf290
Scordo <admin@dev-com.de>
2016-10-02 12:58:12 +0200

    Small refactoring

--- TS3QueryLib.Net.Core/Common/Commands/ExecutableSingleValueCommand.cs
+++ TS3QueryLib.Net.Core/Common/Commands/ExecutableSingleValueCommand.cs
@@ -1,11 +1,23 @@
-﻿using TS3QueryLib.Net.Core.Common.Responses;
+﻿using System;
+using TS3QueryLib.Net.Core.Common.Responses;
 
 namespace TS3QueryLib.Net.Core.Common.Commands
 {
     public class ExecutableSingleValueCommand<TValue> : ExecutableCommand<SingleValueCommandResponse<TValue>>
     {
-        public ExecutableSingleValueCommand(string commandName) : base(commandName)
+        protected string ValueKey { get; }
+
+        public ExecutableSingleValueCommand(string commandName, string valueKey) : base(commandName)
         {
+            if (valueKey == null)
+                throw new ArgumentNullException(nameof(valueKey));
+
+            ValueKey = valueKey;
+        }
+
+        protected override void BeforApplyResponseText(SingleValueCommandResponse<TValue> response, string responseText)
+        {
+            response.ValueKey = ValueKey;
         }
     }
 }
--- TS3QueryLib.Net.Core/Server/Commands/ServerIdGetByPortCommand.cs
+++ TS3QueryLib.Net.Core/Server/Commands/ServerIdGetByPortCommand.cs
@@ -1,18 +1,12 @@
 ﻿using TS3QueryLib.Net.Core.Common.Commands;
-using TS3QueryLib.Net.Core.Common.Responses;
 
 namespace TS3QueryLib.Net.Core.Server.Commands
 {
     public class ServerIdGetByPortCommand : ExecutableSingleValueCommand<uint?>
     {
-        public ServerIdGetByPortCommand(ushort virtualServerPort) : base("ServerIdGetByPort")
+        public ServerIdGetByPortCommand(ushort virtualServerPort) : base("ServerIdGetByPort", "server_id")
         {
             AddParameter("virtualserver_port", virtualServerPort);
-        }
-        
-        protected override void BeforApplyResponseText(SingleValueCommandResponse<uint?> response, string responseText)
-        {
-            response.ValueKey = "server_id";
         }
     }
 }
